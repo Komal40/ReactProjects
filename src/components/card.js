2c9b7cd679de94e7db39b3d14a8de4ed4a01118d
Komal40 <84848693+Komal40@users.noreply.github.com>
2023-04-25 07:38:57 +0530

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ import './card.css'
 export function Card(props){
     return (
         <div className="parent">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV9GDHzUQv-8BKb08F9sPYKgMMPQ6-Lay-VQ&usqp=CAU" className="image"></img>
+            <img src={props.image} className="image"></img>
             <h2 className="name">{props.name}</h2>
             <h2 className="designation">{props.designation}</h2>
             <p className="description">{props.jobDescription}</p>
