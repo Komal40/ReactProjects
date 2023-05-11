a3c02fa638ffe6fbdc2eb59565a52fbdfa1d17f3
unknown <singh777komal@gmail.com>
2023-05-11 09:40:07 +0530

    Make a Scrollbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { GetHeader } from './components/molecules/header/Header';
 import { Section } from './components/organisms/section/section';
+import { Card } from './components/molecules/card/card';
 
 function App() {
   return (
     <>
-    <GetHeader/>
-    <Section/>
+    {/* <GetHeader/>
+    <Section/> */}
+    <Card/>
     </>
   );
 }
